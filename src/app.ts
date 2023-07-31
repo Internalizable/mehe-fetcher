5139c2faab2ac4a38e353340bce18ab8950ce6a3
Elie Sfeir <42282499+Internalizable@users.noreply.github.com>
2023-08-01 02:20:40 +0300

    Fix puppeteer on debian.

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,10 @@ app.get('/', (req, res) => {
 app.listen(port, async () => {
 
     const client = new Client({
-        authStrategy: new LocalAuth()
+        authStrategy: new LocalAuth(),
+        puppeteer: {
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        }
     });
 
     client.on('qr', qr => {
